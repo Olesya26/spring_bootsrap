99f7cfdaef5c1a2c0f94174da12d6b0541b33c74
Olesya26 <metisochka.91@mail.ru>
2021-07-20 11:07:50 +0700

    initial commit

---
+++ src/main/java/ru/kolyadina/spring_boot/model/User.java
@@ -0,0 +1,86 @@
+package ru.kolyadina.spring_boot.model;
+
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "login", unique = true)
+    private String userLogin;
+
+    @NonNull
+    @Column(name = "password")
+    private String passwordUser;
+
+    @NonNull
+    @Column(name = "name")
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NonNull
+    @Column(name = "email")
+    private String email;
+
+
+    @ManyToMany(cascade=CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return getUserLogin();
+    }
+
+    @Override
+    public String getPassword() {
+        return getPasswordUser();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
